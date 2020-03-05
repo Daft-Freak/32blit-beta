e41e91a9694b3e5539edea103e3c399100c6f5a6
Charlie Birks <charlie@daftgames.net>
2020-03-05 12:02:13 +0000

    try experimental

--- 32blit-sdl/File.cpp
+++ 32blit-sdl/File.cpp
@@ -1,11 +1,16 @@
+#if __has_include(<filesystem>)
 #include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
 #include <string>
 
 #include "SDL.h"
 
 #include "File.hpp"
-
-namespace fs = std::filesystem;
 
 static std::string basePath;
 
