6b13d4b90daf976a6c0ade699b207be4f784c1dd
Charlie Birks <charlie@daftgames.net>
2020-06-07 12:37:01 +0100

    Implement basic socket-based send/recv for SDL

--- 32blit-sdl/Main.cpp
+++ 32blit-sdl/Main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 
 #include "Input.hpp"
+#include "Multiplayer.hpp"
 #include "System.hpp"
 #include "Renderer.hpp"
 #include "Audio.hpp"
@@ -28,6 +29,7 @@ SDL_Window* window = nullptr;
 
 System *blit_system;
 Input *blit_input;
+Multiplayer *blit_multiplayer;
 Renderer *blit_renderer;
 Audio *blit_audio;
 
@@ -103,6 +105,7 @@ void handle_event(SDL_Event &event) {
 
 		default:
 			if(event.type == System::loop_event) {
+				blit_multiplayer->update();
 				blit_renderer->update(blit_system);
 				blit_system->notify_redraw();
 				blit_renderer->present();
@@ -133,6 +136,7 @@ void em_loop() {
 		handle_event(event);
 	}
 
+	blit_multiplayer->update();
 	blit_system->loop();
 	blit_renderer->update(blit_system);
 	blit_renderer->present();
@@ -168,6 +172,7 @@ int main(int argc, char *argv[]) {
 
 	blit_system = new System();
 	blit_input = new Input(window, blit_system);
+	blit_multiplayer = new Multiplayer();
 	blit_renderer = new Renderer(window, System::width, System::height);
 	blit_audio = new Audio();
 
@@ -199,6 +204,7 @@ int main(int argc, char *argv[]) {
 
 	blit_system->stop();
 	delete blit_system;
+	delete blit_multiplayer;
 	delete blit_renderer;
 
 	SDL_DestroyWindow(window);
