fea1de8fb40db3e3692afc7bdcb80cf4e9dc3d4e
Charlie Birks <charlie@daft.games>
2022-06-02 15:53:14 +0100

    pico: delay allocating palette

--- 32blit-pico/display.cpp
+++ 32blit-pico/display.cpp
@@ -22,9 +22,8 @@ uint16_t screen_fb[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 uint16_t screen_fb[lores_page_size]; // double-buffered
 #endif
 
-// TODO: this is wasting 1.5k if you're not using paletted modes
-static Pen screen_palette[256];
-uint16_t screen_palette565[256];
+static Pen *screen_palette = nullptr;
+uint16_t *screen_palette565 = nullptr;
 
 static const Size lores_screen_size(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2);
 static const Size hires_screen_size(DISPLAY_WIDTH, DISPLAY_HEIGHT);
@@ -33,6 +32,14 @@ ScreenMode cur_screen_mode = ScreenMode::lores;
 
 int get_display_page_size() {
   return cur_surf_info.bounds.area() * pixel_format_stride[int(cur_surf_info.format)];
+}
+
+static void init_palette() {
+  if(!screen_palette) {
+    // allocate on first use
+    screen_palette = new Pen[256];
+    screen_palette565 = new uint16_t[256]();
+  }
 }
 
 // blit api
@@ -83,6 +90,8 @@ bool set_screen_mode_format(ScreenMode new_mode, SurfaceTemplate &new_surf_templ
 
   if(new_surf_template.format == PixelFormat::P) {
 #ifdef DISPLAY_PICODVI // only handled here so far
+
+    init_palette();
     new_surf_template.palette = screen_palette;
 
     // update converted palette
@@ -106,6 +115,7 @@ bool set_screen_mode_format(ScreenMode new_mode, SurfaceTemplate &new_surf_templ
 }
 
 void set_screen_palette(const Pen *colours, int num_cols) {
+  init_palette();
   memcpy(screen_palette, colours, num_cols * sizeof(Pen));
 
   for(int i = 0; i < num_cols; i++)
