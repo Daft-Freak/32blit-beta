192e0eecafffa5b7956857d7f615f47c1aebc8b0
Charlie Birks <charlie@daft.games>
2023-03-06 22:46:11 +0000

    pico: some kind of basic loader
    Launches the first thing it finds. has some fixed locations for API consts/data.

--- 32blit-pico/main.cpp
+++ 32blit-pico/main.cpp
@@ -171,6 +171,7 @@ static void list_installed_games(std::function<void(const uint8_t *, uint32_t, u
 }
 
 // blit API
+[[gnu::section(".rodata.api_const")]]
 static const blit::APIConst blit_api_const {
   blit::api_version_major, blit::api_version_minor,
 
@@ -233,6 +234,7 @@ static const blit::APIConst blit_api_const {
   ::can_launch,
 };
 
+[[gnu::section(".bss.api_data")]]
 static blit::APIData blit_api_data;
 
 namespace blit {
@@ -308,7 +310,9 @@ int main() {
 #endif
 #endif
 
+#ifndef BUILD_LOADER
   blit::set_screen_mode(ScreenMode::lores);
+#endif
 
   blit::render = ::render;
   blit::update = ::update;
