a4b13cef699c5914f2f1ffc0f0f73e6300ac4d67
Charlie Birks <charlie@daft.games>
2024-08-27 13:52:48 +0100

    pico: call get_audio_frame from "none" audio
    Now anything that depends an audio timing won't hang.

--- 32blit-pico/audio/none.cpp
+++ 32blit-pico/audio/none.cpp
@@ -1,7 +1,17 @@
 #include "audio.hpp"
 
+#include "audio/audio.hpp"
+
+static uint32_t last_update_time = 0;
+
 void init_audio() {
 }
 
 void update_audio(uint32_t time) {
+  uint32_t elapsed = time - last_update_time;
+  last_update_time = time;
+
+  for(auto f = 0u; f < elapsed * blit::sample_rate / 1000; f++) {
+    blit::get_audio_frame();
+  }
 }
