4d0666013d3eea58b9a083b1e2c0798a67adebb8
Charlie Birks <charlie@daft.games>
2024-12-24 17:21:21 +0000

    pico: extend unnamed pad driver for my dual device setup

--- 32blit-pico/input/unnamed_pad.cpp
+++ 32blit-pico/input/unnamed_pad.cpp
@@ -7,7 +7,9 @@
 #include "engine/api_private.hpp"
 #include "engine/input.hpp"
 
+// we only have two possible addresses
 #define I2C_ADDR 0x55
+#define I2C_ADDR2 0x57
 
 // these are based on a vertical layout with the stick at the top
 // which is a bit strange, but what the schematic uses...
@@ -18,11 +20,29 @@
 #define BUTTON_Y_IO      0
 */
 
+#ifdef DUAL_PAD_VERTICAL_EXTRA_BUTTONS
+#define HAVE_SECOND
+#define ROTATE_STICK
+
+#define BUTTON_Y_IO      10
+#define BUTTON_X_IO      12
+#define BUTTON_B_IO      9
+#define BUTTON_A_IO      8
+
+#define BUTTON_RIGHT_IO  2
+#define BUTTON_DOWN_IO   4
+#define BUTTON_UP_IO     1
+#define BUTTON_LEFT_IO   0
+
+#define BUTTON_START_IO  15
+
+#else
 // horizontal layout
 #define BUTTON_X_IO      2
 #define BUTTON_A_IO      4
 #define BUTTON_Y_IO      1
 #define BUTTON_B_IO      0
+#endif
 
 // 5/6 are spare (or should be, but 5 is hacked to LED R)
 #define BUTTON_STICK_IO  7
@@ -34,12 +54,26 @@ void update_input() {
   uint8_t data[4];
   i2c_read_blocking(i2c_default, I2C_ADDR, data, 4, false);
 
-  uint16_t raw_adc0 = data[0] | (data[1] & 0xF) << 8;
-  uint16_t raw_adc1 = data[2] | (data[3] & 0xF) << 8;
-  uint8_t raw_buttons = data[1] >> 4 | (data[3] & 0xF0);
+  uint16_t raw_adc[4];
+  raw_adc[0] = data[0] | (data[1] & 0xF) << 8;
+  raw_adc[1] = data[2] | (data[3] & 0xF) << 8;
+  uint16_t raw_buttons = data[1] >> 4 | (data[3] & 0xF0);
 
-  blit::api_data.joystick.x =  float(raw_adc1 - 0x7FF) / 2047.0f;
-  blit::api_data.joystick.y = -float(raw_adc0 - 0x7FF) / 2047.0f;
+#ifdef HAVE_SECOND
+    i2c_read_blocking(i2c_default, I2C_ADDR2, data, 4, false);
+    raw_adc[2] = data[0] | (data[1] & 0xF) << 8;
+    raw_adc[3] = data[2] | (data[3] & 0xF) << 8;
+    raw_buttons |= (data[1] & 0xF0) << 4 | (data[3] & 0xF0) << 8;
+#endif
+
+#ifdef ROTATE_STICK
+  // this looks like the rotated one, but we're assuming horizontal layout by default...
+  blit::api_data.joystick.x =  float(raw_adc[0] - 0x7FF) / 2047.0f;
+  blit::api_data.joystick.y =  float(raw_adc[1] - 0x7FF) / 2047.0f;
+#else
+  blit::api_data.joystick.x =  float(raw_adc[1] - 0x7FF) / 2047.0f;
+  blit::api_data.joystick.y = -float(raw_adc[0] - 0x7FF) / 2047.0f;
+#endif
 
   uint32_t new_buttons = 0;
 
@@ -55,8 +89,34 @@ void update_input() {
   if(!(raw_buttons & (1 << BUTTON_Y_IO)))
     new_buttons |= blit::Button::Y;
 
+// assume we either have a DPAD or we don't
+#ifdef BUTTON_LEFT_IO
+  if(!(raw_buttons & (1 << BUTTON_LEFT_IO)))
+    new_buttons |= blit::Button::DPAD_LEFT;
+
+  if(!(raw_buttons & (1 << BUTTON_RIGHT_IO)))
+    new_buttons |= blit::Button::DPAD_RIGHT;
+
+  if(!(raw_buttons & (1 << BUTTON_UP_IO)))
+    new_buttons |= blit::Button::DPAD_UP;
+
+  if(!(raw_buttons & (1 << BUTTON_DOWN_IO)))
+    new_buttons |= blit::Button::DPAD_DOWN;
+#endif
+
   if(!(raw_buttons & (1 << BUTTON_STICK_IO)))
     new_buttons |= blit::Button::JOYSTICK;
+
+#ifdef BUTTON_START_IO
+  if(!(raw_buttons & (1 << BUTTON_START_IO)))
+    new_buttons |= blit::Button::HOME;
+#endif
+
+#ifdef DUAL_PAD_VERTICAL_EXTRA_BUTTONS
+  // the last button is on one of the ADC pins
+  if(raw_adc[2] == 0)
+    new_buttons |= blit::Button::MENU;
+#endif
 
   blit::api_data.buttons = new_buttons;
 }
