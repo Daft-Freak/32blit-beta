f99206a272a1f36ae86824d4bf836e22ea2abaef
Charlie Birks <charlie@daft.games>
2024-12-30 15:04:00 +0000

    pico: duplicate some vgaboard config to allow using other PICO_BOARD values
    PICO_BOARD=vgaboard doesn't work so well when you're using a Pico 2

--- 32blit-pico/board/vgaboard/config.cmake
+++ 32blit-pico/board/vgaboard/config.cmake
@@ -1,5 +1,11 @@
 set(BLIT_BOARD_NAME "VGA Board")
 
+set(BLIT_BOARD_DEFINITIONS
+    # these are duplicated so we can build for non-pico boards (vgaboard.h includes pico.h)
+    PICO_SCANVIDEO_COLOR_PIN_BASE=0
+    PICO_SCANVIDEO_SYNC_PIN_BASE=16
+)
+
 blit_driver(audio i2s)
 blit_driver(display dpi)
 blit_driver(input usb_hid)
