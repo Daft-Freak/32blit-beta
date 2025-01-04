56ca191b17e48aed5ce9ab22229614fd7a6ad5f1
Charlie Birks <charlie@daft.games>
2025-01-04 17:20:05 +0000

    pico: PSRAM init code

--- 32blit-pico/main.cpp
+++ 32blit-pico/main.cpp
@@ -20,6 +20,7 @@
 #include "input.hpp"
 #include "led.hpp"
 #include "multiplayer.hpp"
+#include "psram.h"
 #include "storage.hpp"
 #include "usb.hpp"
 
@@ -290,6 +291,10 @@ int main() {
 
   init_usb();
   stdio_init_all();
+
+#ifdef PSRAM_CS_PIN
+  psram_init(PSRAM_CS_PIN);
+#endif
 
   init_i2c();
 
