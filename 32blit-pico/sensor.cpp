58a985bfc0b69472be958539d394efb43645e0f3
Charlie Birks <charlie@daft.games>
2026-01-31 22:44:42 +0000

    pico: make sure api_data.sensors is initialised

--- 32blit-pico/sensor.cpp
+++ 32blit-pico/sensor.cpp
@@ -2,6 +2,8 @@
 
 #include "sensor.hpp"
 
+#include "engine/api_private.hpp"
+
 extern SensorDriver bme280_driver;
 extern SensorDriver lsm6ds3tr_c_driver;
 extern SensorDriver ltr_559als_driver;
@@ -20,6 +22,8 @@ static SensorDriver *sensor_drivers[] {
 static constexpr unsigned num_sensor_drivers = sizeof(sensor_drivers) / sizeof(sensor_drivers[0]);
 
 void init_sensors() {
+  blit::api_data.sensors = nullptr;
+
   for(unsigned i = 0; i < num_sensor_drivers; i++)
     sensor_drivers[i]->init();
 }
