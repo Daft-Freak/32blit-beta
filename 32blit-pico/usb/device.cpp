739679b9f357c89173bfc6f7d1d36945d692f569
Charlie Birks <charlie@daft.games>
2026-03-02 15:22:57 +0000

    pico: use "drive" for msc product suffix instead of "storage"
    There's only 16 chars and I keep getting "Stora"

--- 32blit-pico/usb/device.cpp
+++ 32blit-pico/usb/device.cpp
@@ -17,7 +17,7 @@ void tud_msc_inquiry_cb(uint8_t lun, uint8_t vendor_id[8], uint8_t product_id[16
   (void) lun;
 
   const char vid[] = USB_VENDOR_STR;
-  const char pid[] = USB_PRODUCT_STR " Storage";
+  const char pid[] = USB_PRODUCT_STR " Drive";
   const char rev[] = "1.0";
 
   memcpy(vendor_id  , vid, strlen(vid));
