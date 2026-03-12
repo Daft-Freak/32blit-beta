661bf8d1bb6e87b3c75c1533b21c74f52e49b0fa
Charlie Birks <charlie@daft.games>
2026-03-12 19:56:34 +0000

    pico: add presto board header so we can configure in a more conventaional way
    Stolen from the micropython repo as always

--- 32blit-pico/board/pimoroni_presto/config.cmake
+++ 32blit-pico/board/pimoroni_presto/config.cmake
@@ -1,11 +1,5 @@
 set(BLIT_BOARD_NAME "Presto")
 
-set(BLIT_BOARD_DEFINITIONS
-    PICO_DEFAULT_I2C=0
-    PICO_DEFAULT_I2C_SDA_PIN=40
-    PICO_DEFAULT_I2C_SCL_PIN=41
-)
-
 blit_driver(audio beep)
 blit_driver(display dpi)
 blit_driver(input tca9555)
